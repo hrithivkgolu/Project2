c75d13419af72c9d665f213bde6e9b635beb98b7
hrithivk <hrithivkgo1111@gmail.com>
2025-11-17 20:56:45 +0530

    adding server

---
+++ api/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.responses import JSONResponse
+import json
+import os
+from github import Github
+import requests
+import datetime
+import re
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+
+
+app = FastAPI(title="QUIZ")
+
+
+
+
+@app.get("/")
+async def root():
+    return {
+        "service": "I am here",
+        "status": "OK",
+        "endpoint": "/receive (POST)"
+    }
