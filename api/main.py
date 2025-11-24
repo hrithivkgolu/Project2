68aaf9fdf88c77976fa45b85cf65f527d953cd0b
hrithivk <hrithivkgo1111@gmail.com>
2025-11-25 00:40:51 +0530

    ading ai

--- api/main.py
+++ api/main.py
@@ -2,15 +2,36 @@ from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.responses import JSONResponse
 import json
 import os
-from github import Github
 import requests
 import datetime
 import re
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+from openai import OpenAI
 
 
 app = FastAPI(title="QUIZ")
 
+GOOGLE_FORM_SECRET = "HrithProj2"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+
+def ask_chatgpt(task_content: str, origin: str):
+    prompt = f"""
+You are an automated agent. Read the content below and figure out what JSON
+needs to be submitted. Use this origin:.
+
+CONTENT:
+{task_content}
+
+Return ONLY the JSON body to POST. No explanation.
+"""
+
+    response = client.responses.create(
+        model="gpt-4o-mini",
+        input=prompt
+    )
+
+    return response.output_text
 
 
 
@@ -39,12 +60,13 @@ async def receive(request: Request):
         response = requests.get(data["url"], timeout=10)
         fetched_text = response.text
         fetched_status = response.status_code
+        answer_json = ask_chatgpt(fetched_text)
 
     except Exception as e:
         raise HTTPException(status_code=500, detail = f"Failed to fetch URL: {str(e)}")
 
-
     return JSONResponse(
         status_code=200,
-        content={"status": "ok", "url_status": fetched_status, "content": fetched_text}
+        content={"status": "ok", "url_status": fetched_status, "content": fetched_text, "chatgpt": answer_json}
     )
+
